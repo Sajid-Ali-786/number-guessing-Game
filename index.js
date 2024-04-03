1e8122dd68d7b67b0273d4970e153734ccf15cc6
Sajid Ali Rajper <160535090+Sajid-Ali-786@users.noreply.github.com>
2024-04-03 10:21:44 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([{
+        name: "userGuessedNumber",
+        type: "number",
+        message: "pleaseguessNumber between 1-6"
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number");
+}
+console.log("THE END");
